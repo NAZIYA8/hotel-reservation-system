f4cebc1cf0803d7cf647fbf1d93ae124567fb1c8
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-26 09:04:12 +0530

    Added: A new hotel is added that adds name and regular customer rate

--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -1,13 +1,53 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class is Hotel Reservation class which contains different methods
+ * that can be called.it also contains the main method.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 26-06-2021
+ * ****************************************************************************
+ */
+
 package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class HotelReservation {
 
+    static Scanner scanner = new Scanner(System.in);
+    private static List<Hotel> hotelList = new ArrayList<>();
+
     /**
-     * This is the main method.
+     * This method is used to add Hotel to Hotel Reservation System
+     * that adds name and rate for Regular Customer.
+     */
+    public static void addHotel() {
+        while (true) {
+            System.out.println("Add a hotel:");
+            System.out.println("Enter Hotel Name:");
+            String hotelName = scanner.next();
+            System.out.println("Enter rate for regular customer");
+            int regularCustomerRate = scanner.nextInt();
+            Hotel hotel = new Hotel(hotelName, regularCustomerRate);
+            hotelList.add(hotel);
+            System.out.println("Do You Want to add More hotel(Y/N)");
+            char choice = scanner.next().charAt(0);
+            if (choice != 'Y')
+                break;
+        }
+    }
+
+    /**
+     * This is the main method of the program.
      *
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel reservation system");
+        System.out.println("Welcome to Hotel Reservation System");
+        addHotel();
     }
 }
+
