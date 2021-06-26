a95a24c045df02f7d5e7136b07a9717ac959ddc1
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-26 08:31:24 +0530

    Welcome to Hotel Reservation System

---
+++ src/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class HotelReservation {
+
+    /**
+     * This is the main method.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel reservation system");
+    }
+}
